80134327531a4a3f2d821d1909296db8fcfbefea
Luis Victoria <luisvictoriaperez@gmail.com>
2020-04-12 00:17:37 +0800

    Create main window, display FPS and game time counters

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame
+from defs import *
+
+def update_label(data, title, font, x, y, gameDisplay):
+	label = font.render('{} {}'.format(title, data), 1, data_font_color) # 1 enables anti-aliasing
+	gameDisplay.blit(label, (x,y))
+	return y
+
+def update_data_labels(gameDisplay, dt, game_time, font):
+	y_pos = 10
+	gap = 20
+	x_pos = 10
+	y_pos = update_label(round(1000/dt,2), 'FPS', font, x_pos, y_pos + gap, gameDisplay)
+	y_pos = update_label(round(game_time/1000,2),'Game time', font, x_pos, y_pos + gap, gameDisplay)
+
+
+def run_game():
+	pygame.init()
+	gameDisplay = pygame.display.set_mode((display_W,display_H))
+	pygame.display.set_caption('Learn to fly')
+
+	running = True
+	bgImg = pygame.image.load(bg_filename)
+	label_font = pygame.font.SysFont('monospace',data_font_size)
+
+	clock = pygame.time.Clock()
+	dt = 0
+	game_time = 0
+
+	while running:
+		dt = clock.tick(fps)
+		game_time += dt
+
+		gameDisplay.blit(bgImg, (0,0))
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					running = False
+
+		update_data_labels(gameDisplay, dt, game_time, label_font)
+
+		pygame.display.update()
+
+
+
+if __name__ == '__main__':
+	run_game()
