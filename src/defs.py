d0e282217f361824203a1566985f2cce71b93deb
Luis Victoria <luisvictoriaperez@gmail.com>
2020-04-12 16:03:27 +0800

    Change pipe art and horizontal pipe gap

--- src/defs.py
+++ src/defs.py
@@ -7,18 +7,19 @@ DATA_FONT_COLOR = (40,40,40)
 
 BG_FILENAME = 'src/assets/img/bg.png'
 
-PIPE_FILENAME = 'src/assets/img/pipe.png'
+PIPE_LOWER_FILENAME = 'src/assets/img/pipe_lower.png'
+PIPE_UPPER_FILENAME = 'src/assets/img/pipe_upper.png'
 PIPE_SPEED = 70/1000
 PIPE_DONE = 1
 PIPE_MOVING = 0
 PIPE_UPPER = 1
 PIPE_LOWER = 0
 
-PIPE_ADD_GAP = 160
+PIPE_ADD_GAP = 180 # gap between two different pipes (x)
 PIPE_MIN = 80
 PIPE_MAX = 500
 PIPE_START_X = DISPLAY_W
-PIPE_GAP_SIZE = 160
+PIPE_GAP_SIZE = 160 # space between upper and lower pipes (y)
 PIPE_FIRST = 400 # defines where the first pipe should be (to not have to take a while to start the actual game)
 
 BIRD_FILENAME = 'src/assets/img/bird.png'
@@ -27,4 +28,5 @@ BIRD_START_X = 200
 BIRD_START_Y = 200
 BIRD_ALIVE = 1
 BIRD_DEAD = 0
+
 GRAVITY = 0.001
